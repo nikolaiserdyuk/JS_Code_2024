8f24550db7d9127ea107c1d85e5ad0a9f27fa7b0
Nikolai <flaer645@gmail.com>
2024-03-07 19:52:35 +0300

    solved the second exercise

--- HomeWork_4/1.js
+++ HomeWork_4/1.js
@@ -12,14 +12,14 @@
 
 // Здесь пишем решение, данный комментарий необходимо стереть.
 
-const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let index = 0; index < array.length; index++) {
-  if (index === 0) {
-    console.log(`${index} - это ноль`);
-  } else if (index % 2 !== 0) {
-    console.log(`${index} - нечетное число`);
-  } else {
-    console.log(`${index} - четное чиcло`);
-  }
-}
+// for (let index = 0; index < array.length; index++) {
+//   if (index === 0) {
+//     console.log(`${index} - это ноль`);
+//   } else if (index % 2 !== 0) {
+//     console.log(`${index} - нечетное число`);
+//   } else {
+//     console.log(`${index} - четное чиcло`);
+//   }
+// }
