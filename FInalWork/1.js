9f5093938af28bf66de05daca323220b505c0a43
Nikolai <flaer645@gmail.com>
2024-03-12 20:33:46 +0300

    solved the first exercise

--- FInalWork/1.js
+++ FInalWork/1.js
@@ -16,3 +16,11 @@ const numbers = {
 };
 
 // Здесь пишем решение, данный комментарий необходимо стереть.
+
+const num = [];
+for (const key in numbers) {
+  if (numbers[key] >= 3) {
+    num.push(numbers[key]);
+  }
+}
+console.log(num);
