a26b92015ccada41a9ab2274e768c42ac3ba457b
Nikolai <flaer645@gmail.com>
2024-04-18 12:04:48 +0300

    the end of the second homework

--- HomeWork_F_JS_2/script.js
+++ HomeWork_F_JS_2/script.js
@@ -7,6 +7,21 @@
 // Свойство pages (количество страниц) - число, количество страниц в книге.
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+  displayInfo() {
+    console.log(
+      `Книга под названием ${this.title} от известного писателя ${this.author} имела большой успех, несмотря на то что в ней ${this.pages} страниц.`
+    );
+  }
+}
+const book1 = new Book("Discipline and Punish", "Michel Foucalt", 414);
+book1.displayInfo();
+
 // Задание 2: ""Управление списком студентов""
 // Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
 
@@ -16,15 +31,28 @@
 // Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
 // javascript
 
-// Пример использования класса
-const student1 = new Student(""John Smith"", 16, ""10th grade"");
-student1.displayInfo();
-// Вывод:
-// Name: John Smith
-// Age: 16
-// Grade: 10th grade
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+  displayInfo() {
+    console.log(
+      `One of the students of our school is named ${this.name}, age ${this.age} years old and studies in the ${this.grade}.`
+    );
+  }
+}
 
-const student2 = new Student(""Jane Doe"", 17, ""11th grade"");
+// Пример использования класса
+const student1 = new Student("John Smith", 16, "10th grade");
+student1.displayInfo();
+// // Вывод:
+// // Name: John Smith
+// // Age: 16
+// // Grade: 10th grade
+
+const student2 = new Student("Jane Doe", 17, "11th grade");
 student2.displayInfo();
 // Вывод:
 // Name: Jane Doe
